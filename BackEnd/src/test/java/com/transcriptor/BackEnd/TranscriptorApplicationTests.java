33eb081672249e59f3009804eb6051c009be432f
Juan Martin Raimondo <juanmarraimondo@gmail.com>
2026-03-08 21:25:31 -0300

    Se comienza a configurar el proyecto

---
+++ BackEnd/src/test/java/com/transcriptor/BackEnd/TranscriptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transcriptor.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscriptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
