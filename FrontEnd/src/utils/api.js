b4ed941af767e940d1776753ca427f1d96c8b086
Juan Martin Raimondo <juanmarraimondo@gmail.com>
2026-04-21 12:22:44 -0300

    Cambiar localhost por la IP de produccion en Axios

--- FrontEnd/src/utils/api.js
+++ FrontEnd/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://195.26.254.97',
   headers: {
     'Content-Type': 'application/json'
   }
