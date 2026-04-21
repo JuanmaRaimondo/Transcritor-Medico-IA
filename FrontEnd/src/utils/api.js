5dc4a904abc05f2633d13ae2feffc49cddc204d4
Juan Martin Raimondo <juanmarraimondo@gmail.com>
2026-04-21 12:32:20 -0300

    Agregar puerto 8080 a la URL de Axios

--- FrontEnd/src/utils/api.js
+++ FrontEnd/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://195.26.254.97',
+  baseURL: 'http://195.26.254.97:8080',
   headers: {
     'Content-Type': 'application/json'
   }
